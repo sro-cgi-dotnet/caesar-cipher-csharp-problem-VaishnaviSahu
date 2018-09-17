f2eaa29dfd351f594c1f3339175a05ecb7a8c9bd
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-09-17 10:24:17 +0530

    .net6

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,39 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //Assigning ASCII values to variables
+            int Uppercase = 65;
+            int Lowercase = 97;
+
+            // Creating a New string to return
+            string Cipher = "";
+
+            // Loop to read all characters in string
+            foreach(char c in text)
+            {
+                // UpperCase Letters
+                if(char.IsUpper(c))
+                {
+                    int a = (int)c - Uppercase + shiftKey;
+                    a %= 26;
+                    a += Uppercase;
+                    Cipher += (char)a;
+                }
+                // LowerCase Letters
+                else if(char.IsLower(c))
+                {
+                    int a = (int)c - Lowercase + shiftKey;
+                    a %= 26;
+                    a += Lowercase;
+                    Cipher += (char)a;
+                }
+                // Other Characters
+                else
+                {
+                    Cipher += c;
+                }
+            }
+            return Cipher;
         }
     }
 }
